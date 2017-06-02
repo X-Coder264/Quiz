d40af69814438c089ad187b81bd78cd70c532536
Mario Sunc <mario.sunc@gmail.com>
2017-06-02 03:16:51 +0200

    Login, Registration, Main, Play mode screens - Added elements

--- app/src/main/java/hr/tvz/quiz/SignUpActivity.java
+++ app/src/main/java/hr/tvz/quiz/SignUpActivity.java
@@ -38,7 +38,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_registration);
 
         userLocalStore = new UserLocalStore(this);
 
